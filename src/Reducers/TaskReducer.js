8e56e13cef345be1b1f869052fe258ae2d32b4c5
tduong992 <tduong992@gmail.com>
2018-05-11 00:25:46 +0200

    replace the fake hardcoded server response with the actual action.payload (response)

--- src/Reducers/TaskReducer.js
+++ src/Reducers/TaskReducer.js
@@ -7,39 +7,44 @@ import {
 const initialState = {
     isFetching: false,
     tasks: [{
+        id: '0123',
         title: 'Throw garbage',
-        effort: 'NORMAL',
+        effort: 1,
         frequency: 'DAILY',
-        start: new Date(),
-        users: ['Alice', 'Bob', 'Chris', 'Dave'],
-        // usersRotation: ['Alice', 'Bob', 'Chris', 'Dave'],
-    },
-        /*{
-        title: 'Buy groceries',
-        effort: 'NORMAL',
-        frequency: 'WEEKLY',
-        start: new Date(),
+        start: new Date().setFullYear(2018,4,9),
+        // users: ['Jane', 'Tom', 'Joe'],
+        usersRotation: ['Alice', 'Bob', 'Chris', 'Dave'],
     },
         {
-        title: 'Clean house',
-        effort: 'HIGH',
+        id: '1234',
+        title: 'Buy groceries',
+        effort: 1,
+        frequency: 'WEEKLY',
+        start: new Date().setFullYear(2018,4,16),
+            usersRotation: ['Tom', 'Joe', 'Jane'],
+    },
+        {
+        id: '2345',
+        title: 'Clean bathroom',
+        effort: 2,
         frequency: "WEEKLY",
-        start: new Date()
-    }*/
+        start: new Date().setFullYear(2018,4,19),
+            usersRotation: ['Joe', 'Tom', 'Jane'],
+    }
     ],
     hasError: false,
     errorMessage: null,
 };
 
-function guid() {
-    function s4() {
-        return Math.floor((1 + Math.random()) * 0x10000)
-            .toString(16)
-            .substring(1);
-    }
-    
-    return `${s4()}${s4()}-${s4()}-${s4()}-${s4()}-${s4()}${s4()}${s4()}`;
-}
+// function guid() {
+//     function s4() {
+//         return Math.floor((1 + Math.random()) * 0x10000)
+//             .toString(16)
+//             .substring(1);
+//     }
+//
+//     return `${s4()}${s4()}-${s4()}-${s4()}-${s4()}-${s4()}${s4()}${s4()}`;
+// }
 
 export default function (state = initialState, action) {
 
@@ -47,29 +52,36 @@ export default function (state = initialState, action) {
 
         case FETCHING_TASKS:
             return Object.assign({}, state, {
+                ...state,
                 isFetching: true,
-                tasks: [],
+                // tasks: state.tasks,
                 hasError: false,
                 errorMessage: null
             });
 
         case FETCHING_TASKS_SUCCESS:
+            console.log('FETCHING_TASKS_SUCCESS');
+            console.log(action.payload);
             // Create a list of tasks with an ID.
-            const tasks = action.payload.map((task) => {
-                //TODO: replace with actual ids
-                task.id = guid();
-                return task;
-            });
+            // const tasks = action.payload.map((task) => {
+            // const tasks = state.tasks.map((task) => {
+            //     //TODO: replace with actual ids
+            //     task.id = guid();
+            //     return task;
+            // });
 
             return Object.assign({}, state, {
+                ...state,
                 isFetching: false,
-                tasks: tasks,
+                tasks: action.payload,
+                // tasks: state.tasks,
                 hasError: false,
                 errorMessage: null
             });
 
         case FETCHING_TASKS_FAIL:
             return Object.assign({}, state, {
+                ...state,
                 isFetching: false,
                 tasks: action.payload,
                 hasError: true,
--- src/Reducers/UserReducer.js
+++ src/Reducers/UserReducer.js
@@ -6,37 +6,38 @@ import {
     USER_LOGGED_IN,
     USER_LOGGING_IN,
 } from './../Utils/ActionTypes';
+import { AsyncStorage } from 'react-native';
 
 const initialState = {
     isFetching: false,
     currentUser: null,
     isLoggingIn: false,
     users: [{
-        id: guid(),
+        id: '0123',
         nickname: 'Jane',
         score: 6,
     },{
-        id: guid(),
+        id: '1234',
         nickname: 'Tom',
         score: 5,
     },{
-        id: guid(),
+        id: '2345',
         nickname: 'Joe',
-        score: 3,
+        score: 4,
     },],
     hasError: false,
     errorMessage: null,
 };
 
-function guid() {
-    function s4() {
-        return Math.floor((1 + Math.random()) * 0x10000)
-            .toString(16)
-            .substring(1);
-    }
-    
-    return `${s4()}${s4()}-${s4()}-${s4()}-${s4()}-${s4()}${s4()}${s4()}`;
-}
+// function guid() {
+//     function s4() {
+//         return Math.floor((1 + Math.random()) * 0x10000)
+//             .toString(16)
+//             .substring(1);
+//     }
+//
+//     return `${s4()}${s4()}-${s4()}-${s4()}-${s4()}-${s4()}${s4()}${s4()}`;
+// }
 
 export default function (state = initialState, action) {
 
@@ -48,17 +49,23 @@ export default function (state = initialState, action) {
             });
         
         case USER_LOGGED_IN:
-            const currentUser = action.currentUser;
+            console.log('USER_LOGGED_IN');
+            console.log(action.currentUser);
             return Object.assign({}, state, {
                 ...state,
-                currentUser: currentUser,
+                currentUser: action.currentUser,
                 isLoggingIn: false,
             });
 
         case FETCHING_USERS:
+            console.log('FETCHING_USERS');
+            console.log(action);
+            
+            const currentUser = state.currentUser ? state.currentUser : AsyncStorage.getItem('currentUserID');
             return Object.assign({}, state, {
+                ...state,
                 isFetching: true,
-                currentUser: state.users[0],
+                currentUser: currentUser ? currentUser : state.users[0],
                 users: state.users,
                 // users: [],
                 hasError: false,
@@ -66,26 +73,24 @@ export default function (state = initialState, action) {
             });
 
         case FETCHING_USERS_SUCCESS:
-            const users = action.payload.map((user) => {
-                //TODO: replace with actual ids
-                user.id = guid();
-                return user;
-            });
+            console.log('FETCHING_USERS_SUCCESS');
+            console.log(action.payload);
             
             return Object.assign({}, state, {
+                ...state,
                 isFetching: false,
-                //users: action.payload,
-                //users: users,
-                users: state.users,
+                users: action.payload,
+                // users: state.users,
                 hasError: false,
                 errorMessage: null
             });
 
         case FETCHING_USERS_FAIL:
             return Object.assign({}, state, {
+                ...state,
                 isFetching: false,
-                //users: action.payload,
-                users: state.users,
+                users: action.payload,
+                // users: state.users,
                 hasError: true,
                 errorMessage: action.err
             });
