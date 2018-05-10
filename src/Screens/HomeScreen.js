394162d5f3244562f20b260e97c28e9bf9dc8277
tduong992 <tduong992@gmail.com>
2018-05-11 00:45:23 +0200

    set currentUser and users (roommates) in HomeScreen.js state

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,10 +1,11 @@
 import { connect } from 'react-redux';
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, AsyncStorage } from 'react-native';
 import Header from '../components/Header';
 import TaskContainer from '../components/TaskContainer';
 import HallOfFameContainer from "../components/HallOfFameContainer";
 import { FetchUsers } from './../Actions/FetchUsers';
+import { FetchTasks } from './../Actions/FetchTasks';
 
 class HomeScreen extends Component {
     // Sets title of "tab"
@@ -14,9 +15,25 @@ class HomeScreen extends Component {
     
     constructor(props) {
         super(props);
-        
+    
+        console.log('HOME props ------');
+        console.log(this.props);
+        const {currentUser} = this.props;
+        const {roommates} = this.props;
+        // const {tasks} = this.props;
+        console.log(currentUser);
+        console.log(this.props);
+        AsyncStorage.setItem('currentUserNickname', currentUser ? currentUser.nickname : '');
+        AsyncStorage.setItem('currentUserScore', currentUser ? currentUser.score : 'NaN');
+        // AsyncStorage.setItem('roommates', roommates);
+    
+        // console.log(AsyncStorage.getItem('currentUser'));
+        // console.log(AsyncStorage.getItem('roommates'));
         this.state = {
-            tabSelected: 'Overview'
+            tabSelected: 'Overview',
+            currentUser: currentUser,
+            roommates: roommates,
+            // tasks: tasks,
         };
         
         this.onTabPressed = this.onTabPressed.bind(this);
@@ -37,6 +54,7 @@ class HomeScreen extends Component {
                 tabSelected={tabSelected}
                 onTabPressed={this.onTabPressed}
                 onCreateTaskPressed={() => this.onCreateTaskPressed()}
+                currentUser={currentUser}
                 score={score}
             />
         );
@@ -45,6 +63,7 @@ class HomeScreen extends Component {
     onTabPressed(tabSelected) {
         console.log(tabSelected);
         this.setState({
+            ...this.state,
             tabSelected: tabSelected
         })
     }
@@ -70,9 +89,23 @@ class HomeScreen extends Component {
 }
 
 function mapStateToProps(state) {
+    console.log('HOME SCREEN state ---');
+    console.log(state);
+    
+    // this.props.FetchTasks();
     return {
-        currentUser: state.usersData.currentUser
+        // tasks: state.taskData.tasks,
+        currentUser: state.usersData.currentUser,
+        roommates: state.usersData.users,
     }
 }
 
+// const mapDispatchToProps = {
+//     FetchUsers,
+//     // FetchTasks,
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(HomeScreen)
+
+// export default connect(mapStateToProps, { FetchUsers, FetchTasks })(HomeScreen)
 export default connect(mapStateToProps, { FetchUsers })(HomeScreen)
