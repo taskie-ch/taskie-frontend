f2e5c76d86934b65c5d430cae3c338f7311c7ed3
tduong992 <tduong992@gmail.com>
2018-05-13 19:36:18 +0200

    linking action/AJAX functions with the task UI buttons

--- src/Screens/CreateTaskScreen.js
+++ src/Screens/CreateTaskScreen.js
@@ -1,7 +1,9 @@
 import { connect } from 'react-redux';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View } from 'react-native';
 import React, { Component } from 'react';
 import CreateTaskForm from '../components/CreateTaskForm';
+import { FetchTasks, PostTask, DeleteTask } from '../Actions/FetchTasks';
+// import styles from './../styles';
 
 
 class CreateTaskScreen extends Component {
@@ -16,17 +18,49 @@ class CreateTaskScreen extends Component {
         super(props);
 
         this.state = {
-            id: "",
-            title: "",
-            frequency: "",
-            start: "",
-            effort: "",
-            done: "",
+            id: '',
+            title: '',
+            frequency: '',
+            start: '',
+            effort: '',
+            done: '',
             // users: [],
             usersRotation: [],
         };
         this.handleFormSubmit = this.handleFormSubmit.bind(this);
+        this.handleDeleteTaskSubmit = this.handleDeleteTaskSubmit.bind(this);
     }
+
+    handleDeleteTaskSubmit(value) {
+        console.log("DELETE TASK submit -------");
+        console.log(value.title);
+        console.log(value.id);
+        // console.log('posted task ' + this.id);
+        // value.id ? this.props.PostTask(value) : this.props.PostTask(value);
+        // this.props.PostTask(value);
+        // this.props.FetchTasks();
+    
+        this.deleteNavigateToApp(value);
+        // const navigator = this.props.navigation;
+        // navigator.navigate('App');
+    }
+    
+    deleteNavigateToApp = async (value) => {
+        
+        console.log('deleting task ' + value.id);
+        const res = await this.props.DeleteTask(value.id);
+        console.log('delete res');
+        console.log(res);
+        // console.log('post fetching tasks');
+        await this.props.FetchTasks();
+        // console.log("Dispatch post fetching tasks");
+        // console.log(this.props);
+        // console.log(this.props.navigation);
+        // console.log(this.props.navigator);
+        // console.log('navigating to app');
+        this.props.navigation.navigate('Home');
+        // this.props.navigator.navigate('Home');
+    };
 
     handleFormSubmit(value) {
         console.log("CREATE TASK SCREEN submit -------");
@@ -34,6 +68,7 @@ class CreateTaskScreen extends Component {
         this.setState(
             {
                 ...this.state,
+                id: value.id ? value.id : '',
                 title: value.title,
                 frequency: value.frequency,
                 start: value.start,
@@ -42,7 +77,32 @@ class CreateTaskScreen extends Component {
                 // users: value.users,
                 usersRotation: value.usersRotation,
             });
+        // console.log('posted task ' + this.id);
+        // value.id ? this.props.PostTask(value) : this.props.PostTask(value);
+        // this.props.PostTask(value);
+        // this.props.FetchTasks();
+    
+        this.navigateToApp(value);
+        // const navigator = this.props.navigation;
+        // navigator.navigate('App');
     }
+    
+    navigateToApp = async (value) => {
+        
+        console.log('posting task ' + this.state.id);
+        const res = await this.props.PostTask(value);
+        console.log('post res');
+        console.log(res);
+        // console.log('post fetching tasks');
+        await this.props.FetchTasks();
+        // console.log("Dispatch post fetching tasks");
+        // console.log(this.props);
+        // console.log(this.props.navigation);
+        // console.log(this.props.navigator);
+        // console.log('navigating to app');
+        this.props.navigation.navigate('Home');
+        // this.props.navigator.navigate('Home');
+    };
 
     setUsersRotationObject(task) {
         const users = task.usersRotation;
@@ -59,7 +119,7 @@ class CreateTaskScreen extends Component {
 
     getTaskById(id) {
         let task = this.props.tasks.filter(task => task.id === id);
-        if (task) {
+        if (task.length) {
             console.log('GET TASK by ID');
             // console.log(task);
             task = this.setUsersRotationObject(task[0]);
@@ -72,11 +132,13 @@ class CreateTaskScreen extends Component {
         console.log('create task screen RENDER');
         // console.log('this.state');
         // console.log(this.state);
-        console.log(this.props.navigation.state);
+        // console.log(this.props);
+        // console.log(this.props.navigation.state);
         let {roommates} = this.props;
         // console.log('ROOMMATES');
         // console.log(roommates);
-        let {title: title, frequency: frequency, start: start, effort: effort, done: done, users: users} = this.state;
+        let {title: title, frequency: frequency, start: start, effort: effort, done: done, usersRotation: usersRotation} = this.state;
+        // start = start ? start.toLocaleDateString() : '';
         if (start) {
             start = start.toLocaleDateString();
         }
@@ -85,6 +147,7 @@ class CreateTaskScreen extends Component {
         console.log('Task ID ------');
         console.log(id);
         let task = id ? this.getTaskById(id) : null;
+        console.log(task);
         // console.log('Just for debug');
         // console.log(task);
         // console.log(task.title);
@@ -95,9 +158,9 @@ class CreateTaskScreen extends Component {
         // console.log(usersRotation);
         return (
             <View>
-                <CreateTaskForm task={task} roommates={roommates} onFormSubmit={this.handleFormSubmit}/>
+                <CreateTaskForm task={task} roommates={roommates} onFormSubmit={this.handleFormSubmit} onDeleteSubmit={this.handleDeleteTaskSubmit}/>
 
-                {/*<View style={container}>*/}
+                {/*<View style={styles.createTaskScreenDebugContainer}>*/}
                     {/*<Text style={{color: "red"}}>Just for debug</Text>*/}
                     {/*<Text>Title: {title}</Text>*/}
                     {/*<Text>Frequency: {frequency}</Text>*/}
@@ -110,16 +173,6 @@ class CreateTaskScreen extends Component {
     }
 }
 
-/* Define some CSS rules */
-const styles = StyleSheet.create({
-    container: {
-        marginTop: 10,
-        marginLeft: 20,
-    },
-});
-
-const { container } = styles;
-
 /* Bind the Store's state to CreateTaskScreen props */
 function mapStateToProps(state) {
     console.log('CREATE task SCREEN state ---');
@@ -130,4 +183,10 @@ function mapStateToProps(state) {
     }
 }
 
-export default connect(mapStateToProps, {})(CreateTaskScreen);
+const mapDispatchToProps = {
+    FetchTasks,
+    PostTask,
+    DeleteTask,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CreateTaskScreen);
--- src/components/TaskContainer.js
+++ src/components/TaskContainer.js
@@ -3,43 +3,137 @@ import { connect } from 'react-redux';
 import React, { Component } from 'react';
 import { ScrollView, View } from 'react-native';
 
-import FetchTasks from './../Actions/FetchTasks';
+import { FetchUsers } from './../Actions/FetchUsers';
+import { FetchTasks, PostTask, DeleteTask, SkipTask, DoneTask } from './../Actions/FetchTasks';
 import TaskCard from './TaskCard';
+import styles from './../styles';
 
 
 class TaskContainer extends Component {
+    
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+
+        this.onTaskDone = this.onTaskDone.bind(this);
+        this.onTaskSkip = this.onTaskSkip.bind(this);
+        this.onTaskIWillDoIt = this.onTaskIWillDoIt.bind(this);
+    }
 
     componentDidMount() {
         this.props.FetchTasks();
     }
+    
+    onTaskDone(task) {
+        console.log("Done task: " + task.title);
+        console.log("Done task ID: " + task.id);
+        this.props.DoneTask(task.id, task.usersRotation[0]);
+        console.log('done');
+        this.doneNavigateToApp();
+    }
+    
+    doneNavigateToApp = async () => {
+        console.log('done fetching tasks');
+        await this.props.FetchTasks();
+        console.log("Dispatch done fetching tasks");
+        // console.log(this.props);
+        console.log('navigating to app');
+        this.props.navigator.navigate('App');
+    };
+    
+    onTaskIWillDoIt(task) {
+        console.log("Done task: " + task.title);
+        console.log("Done task ID: " + task.id);
+        this.props.DoneTask(task.id, task.usersRotation[0]);
+        console.log('done');
+        this.iWillDoItNavigateToApp();
+    }
+    
+    iWillDoItNavigateToApp = async () => {
+        console.log('done fetching tasks');
+        await this.props.FetchTasks();
+        console.log("Dispatch done fetching tasks");
+        // console.log(this.props);
+        console.log('navigating to app');
+        this.props.navigator.navigate('App');
+    };
+
+    onTaskSkip(task) {
+        console.log("skip task: " + task.title);
+        console.log("skip task ID: " + task.id);
+        this.props.SkipTask(task.id, task.usersRotation[0]);
+        console.log('skip');
+        this.skipNavigateToApp();
+    }
+    
+    skipNavigateToApp = async () => {
+        console.log('skip fetching tasks');
+        await this.props.FetchUsers();
+        console.log("Dispatch skip fetching tasks");
+        // console.log(this.props);
+        console.log('navigating to app');
+        this.props.navigator.navigate('App');
+    };
+
+    onTaskDelete(task) {
+        console.log("Deleting task: " + task.title);
+        console.log("Deleting task ID: " + task.id);
+        this.props.DeleteTask(task.id);
+        console.log('deleted');
+        this.navigateToApp();
+    }
+    
+    navigateToApp = async () => {
+    
+        console.log('delete fetching tasks');
+        await this.props.FetchTasks();
+        console.log("Dispatch delete fetching tasks");
+        // console.log(this.props);
+        console.log('navigating to app');
+        this.props.navigator.navigate('App');
+    };
 
     onTaskCardPressed(id) {
         this.props.navigator.navigate('CreateTask', {id: id});
     }
 
     renderTaskCards() {
+        const currentUser = this.props.currentUser;
+        const roommates = this.props.roommates;
         let tasks = this.props.tasks;
-        tasks.sort(function(task1, task2){return new Date(task1.start) - new Date(task2.start)});
+        console.log('RENDER TASK CARD');
+        // console.log(this.props);
+        console.log(tasks);
+        tasks = (tasks.length > 0) ? tasks.sort(function(task1, task2){return new Date(task1.start) - new Date(task2.start)}) : [];
         return tasks.map(task => {
-                return <TaskCard
-                    key={`${task.id}`}
-                    id={task.id}
-                    title={task.title}
-                    // effort={task.effort}
-                    // frequency={task.frequency}
-                    start={task.start ? new Date(task.start).toDateString() : 'Monday'}
-                    done={task.done}
-                    user={task.users.length ? task.users[0].toUpperCase() : 'Nickname'}
-                    // user={task.usersRotation.length ? task.usersRotation[0].toUpperCase() : 'Nickname'}
-                    taskCardPressed={() => this.onTaskCardPressed(task.id)}
-                />
-            }
-        )
+            console.log('renderTaskCards render return');
+            // console.log(task);
+            console.log(task.usersRotation[0]);
+            console.log('Roommates');
+            console.log(roommates);
+            const currentUser = currentUser ? currentUser : roommates[0];
+            console.log(currentUser);
+            const user = roommates.filter(roomie => roomie.id === task.usersRotation[0]);
+            console.log(user);
+            return <TaskCard
+                key={`${task.id}`}
+                id={task.id}
+                title={task.title}
+                start={task.start ? new Date(task.start).toDateString() : 'Monday'}
+                done={task.done}
+                user={user.length ? user[0].nickname.toUpperCase() : 'Nickname'}
+                currentUser={currentUser}
+                taskCardPressed={() => this.onTaskCardPressed(task.id)}
+                taskDone={() => this.onTaskDone(task)}
+                taskSkip={() => this.onTaskSkip(task)}
+                taskIWillDoIt={() => this.onTaskIWillDoIt(task)}
+            />
+        })
     }
 
     render() {
         const {tasks} = this.props;
-        const {contentContainer} = styles;
 
         if (tasks.isFetching) {
             return (
@@ -55,24 +149,29 @@ class TaskContainer extends Component {
         }
 
         return (
-            <ScrollView style={ contentContainer }>
+            <ScrollView style={ styles.taskContainerBody }>
                 {this.renderTaskCards()}
             </ScrollView>
         )
     }
 }
 
-const styles = {
-    contentContainer: {
-        paddingBottom: 100,
-        marginTop: 70,
-    }
-};
-
 function mapStateToProps(state) {
+    console.log('TASK Container state ---');
+    console.log(state);
     return {
-        tasks: state.taskData.tasks
+        tasks: state.taskData.tasks,
+        currentUser: state.usersData.currentUser,
+        roommates: state.usersData.users,
     }
 }
 
-export default connect(mapStateToProps, { FetchTasks })(TaskContainer)
+const mapDispatchToProps = {
+    FetchUsers,
+    FetchTasks,
+    DeleteTask,
+    SkipTask,
+    DoneTask,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(TaskContainer)
