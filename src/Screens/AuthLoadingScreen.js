acbe8767ded2658d0f82563c189d07cf9310d166
tduong992 <tduong992@gmail.com>
2018-05-14 20:18:50 +0200

    let the sign in form be displayed

--- src/Screens/AuthLoadingScreen.js
+++ src/Screens/AuthLoadingScreen.js
@@ -10,9 +10,9 @@ export default class AuthLoadingScreen extends Component {
     
     // Fetch the token from storage then navigate to our appropriate place
     _bootstrapAsync = async () => {
-        const userToken = await AsyncStorage.getItem('currentUserID');//null;//
-        console.log('USER TOKEN ?');
-        console.log(userToken);
+        const userToken = null;//await AsyncStorage.getItem('currentUserID');//null;//
+        // console.log('USER TOKEN ?');
+        // console.log(userToken);
         
         // This will switch to the App screen or Auth screen and this loading
         // screen will be unmounted and thrown away.
